9c64e0ee010c62a3f6771a9373dd0ddd1c86b26f
VenoMKO <VenoMKO@users.noreply.github.com>
2020-11-20 23:01:50 +0300

    Bump App version

--- Core/Tera/Core.h
+++ Core/Tera/Core.h
@@ -8,7 +8,7 @@
 // Build configuration
 // --------------------------------------------------------------------
 
-static const float APP_VER = 1.52f;
+static const float APP_VER = 1.53f;
 //#define CUSTOM_BUILD " RC"
 
 #define WIN_POS_FULLSCREEN INT_MIN
